f0bdf12f7c233703eb00f3709a22ae9b42f9eeb8
YEUNG Ching Fung <cfyeung9@csci2720.cse.cuhk.edu.hk>
2020-07-26 15:59:48 +0800

    some updates

--- src/App.js
+++ src/App.js
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello World!</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+constructor(props) {
+super(props);
+this.state = { apitext: '' };
 }
+componentDidMount() {
+fetch(" Your API endpoint URL ")
+.then(res => res.json())
+.then(
+(result) => {
+this.setState({
+apitext: result.text
+});
+});
+}
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code>{this.state.apitext} </code>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+    }
+  }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+//some updates
   <React.StrictMode>
     <App />
   </React.StrictMode>,
